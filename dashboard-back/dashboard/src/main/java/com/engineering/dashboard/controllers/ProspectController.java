b2195dbd00f55fc05d6ab29d7bf4e1960cec8a1f
lgonzalezvalencia <lgonzalezvalencia@deloitte.com>
2025-03-26 13:37:08 -0600

    Added Prospect Controller and Service

--- dashboard-back/dashboard/src/main/java/com/engineering/dashboard/controllers/ProspectController.java
+++ dashboard-back/dashboard/src/main/java/com/engineering/dashboard/controllers/ProspectController.java
@@ -1,5 +1,50 @@
 package com.engineering.dashboard.controllers;
 
-public class ProspectController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.engineering.dashboard.entities.ProspectEntity;
+import com.engineering.dashboard.services.ProspectService;
+
+@RestController
+@RequestMapping("/api/v1/prospects")
+public class ProspectController {
+	@Autowired
+    ProspectService prospectService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ProspectEntity>> getProspects() {
+        return prospectService.getAllProspects();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ProspectEntity> postProspect(@RequestBody ProspectEntity prospect) {
+        return prospectService.createProspect(prospect);
+    }
+
+    @GetMapping("/{prospectId}")
+    public ResponseEntity<ProspectEntity> getProspect(@RequestParam Long prospectId) {
+        return prospectService.getProspect(prospectId);
+    }
+
+    @PutMapping("/{prospectId}")
+    public ResponseEntity<ProspectEntity> putProspect(@PathVariable Long prospectId, @RequestBody ProspectEntity prospect) {
+        return prospectService.updateProspect(prospectId,prospect);
+    }
+
+    @DeleteMapping("/{prospectId}")
+    public ResponseEntity<ProspectEntity> deleteProspect(@PathVariable Long prospectId){
+        return prospectService.deleteProspect(prospectId);
+    }
 }
