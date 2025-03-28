1eb6c7c8fda31de3bdef6a59781b2f2f0f9c2ef2
jfrodriguezhD <frodriguezherrera@deloitte.com>
2025-03-28 12:38:45 -0600

    correcting styles

--- dashboard-front/src/components/molecules/roster_table/Roster_Table.tsx
+++ dashboard-front/src/components/molecules/roster_table/Roster_Table.tsx
@@ -1,35 +1,47 @@
-import Roster_Footer from "../../atoms/Roster_Footer/Roster_Footer";
-import Roster_Row from "../../atoms/Roster_Row/Roster_Row.tsx";
+import Roster_Footer from "../../atoms/roster_footer/Roster_Footer";
+import Roster_Row from "../../atoms/roster_row/Roster_Row";
 import "./Roster_Table.css"; 
 import data from "./roster_example.json";
 
 function Roster_Table() {
-  console.log(data);
-  return (
-    <>
-      <div className="roster-table">
-        <div className="table-header">
-          <p className="roster__name__cell">Name</p>
-          <p className="roster__cell">Status</p>
-          <p className="roster__cell">Level</p>
-          <p className="roster__cell">Expertise</p>
-          <p className="roster__cell">Capability</p>
-          <p className="roster__cell">Project</p>
-          <p className="roster__cell">Prospected for</p>
-        </div>
+	const addMember = () =>{
+		console.log("new user");
+	}
+
+  	return (
+    	<>
+      	<div className="roster-table">
+        	<div className="table-header">
+          	<p className="roster__name__cell">Name</p>
+			<p className="roster__cell">Status</p>
+			<p className="roster__cell">Level</p>
+			<p className="roster__cell">Expertise</p>
+			<p className="roster__cell">Capability</p>
+			<p className="roster__cell">Project</p>
+			<p className="roster__cell">Prospected for</p>
+		</div>
         {data.map((member) => (
-          <Roster_Row
-            id={member.id}
-            firstName={member.firstname}
-            lastName={member.lastname}
-            status={member.status}
-            level={member.seniority}
-            expertise={member.job_title}
-            capability={member.capability}
-            project={member.project}
-            prospectedFor={member.prospected_for}
-          />
+        	<Roster_Row
+				id={member.id}
+				firstName={member.firstname}
+				lastName={member.lastname}
+				status={member.status}
+				level={member.seniority}
+				expertise={member.job_title}
+				capability={member.capability}
+				project={member.project}
+				prospectedFor={member.prospected_for}
+          	/>
         ))}
+        <div className='roster-member__new' onClick={addMember}>
+			<p className='roster__name__cell'>Add new member to Roster...</p>
+			<p className="roster__cell"></p>
+			<p className="roster__cell"></p>
+			<p className="roster__cell"></p>
+			<p className="roster__cell"></p>
+			<p className="roster__cell"></p>
+			<p className="roster__cell"></p>
+        </div>
       </div>
       <Roster_Footer len={Math.floor(data.length / 10)} selected={1} />
     </>
