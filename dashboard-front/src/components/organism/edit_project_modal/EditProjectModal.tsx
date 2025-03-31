a8633c7d2b38ddbc828108e1ff8fe15aa7a0ad5e
J-Sanchez21 <jsanchezibarra@deloitte.com>
2025-03-31 12:38:39 -0600

    Refactorizing EditProjectModal Window

--- dashboard-front/src/components/organism/edit_project_modal/EditProjectModal.tsx
+++ dashboard-front/src/components/organism/edit_project_modal/EditProjectModal.tsx
@@ -1,36 +1,47 @@
 import WordBubble from "../../atoms/wordbubble/WordBubble";
-import "./EditProjectModal.css"
-import { forwardRef, useState } from "react"
+import "./EditProjectModal.css";
+import { useState } from "react";
 
-interface Props {
-    toggleDialog: () => void;
-  }
+interface ProyectInfoEditProps {
+  closeModal: () => void;
+}
 
+function EditProjectModal({ closeModal }: ProyectInfoEditProps) {
+  // Supposed to get 5 last projects on initialization, changes when a search is called
+  const [projectList, setProjectList] = useState<string[]>([
+    "Amazon",
+    "Google",
+    "Toyota",
+    "Nissan",
+    "Nasa",
+  ]);
 
-export default forwardRef<HTMLDialogElement, Props>(function EditProfileModal(
-  { toggleDialog },
-  ref
-) {
-    //Supposed to get 5 last projects on initialization, changes when a search is called
-    const [projectList,setProjectList] = useState<string[]>(["Amazon","Google","Toyota","Nissan","Nasa"])
-    return (
-        <dialog ref={ref} className="edit-project-modal">
-            <div className="edit-project-modal__container">
-                <div className="edit-project-modal__heading">
-                    <h2>Edit Project Information</h2>
-                    <button className="close-modal" onClick={() => toggleDialog()}>
-                    X
-                    </button>
-                </div>
+  return (
+    <div className="edit-project-modal__container">
+      <div className="edit-project-modal__heading">
+        <h2>Edit Project Information</h2>
+        <button className="close-edit-project-modal" onClick={closeModal}>
+          X
+        </button>
+      </div>
 
-                <input className="edit-project__searchbar" placeholder="Insert a project name"/>
-                <button className="edit-project__searchbutton">Search</button>
-                <div className="edit-project__list">
-                    
-                    {projectList.map((project) => <WordBubble group="projects" type="radio" word={project} key={project}/>)}
-                </div>
-                <button className="edit-project__submitbutton">Save</button>
-            </div>
-        </dialog>
-    )
-})
+      <input
+        className="edit-project__searchbar"
+        placeholder="Insert a project name"
+      />
+      <button className="edit-project__searchbutton">Search</button>
+      <div className="edit-project__list">
+        {projectList.map((project) => (
+          <WordBubble
+            group="projects"
+            type="radio"
+            word={project}
+            key={project}
+          />
+        ))}
+      </div>
+      <button className="edit-project__submitbutton">Save</button>
+    </div>
+  );
+}
+export { EditProjectModal };
