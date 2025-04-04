34e78c0e45486bca552311e8021957e692cb9229
jfrodriguezhD <frodriguezherrera@deloitte.com>
2025-04-04 10:14:39 -0600

    Implement dark/light mode

--- dashboard-front/src/components/molecules/header/Header.tsx
+++ dashboard-front/src/components/molecules/header/Header.tsx
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./Header.css";
 import HamburguerMenu from "../hamburguer_menu/HamburguerMenu";
 
 function Header() {
   const [isMenuVisible, setMenuVisible] = useState(false);
+  const [darkMode, setDarkMode] = useState<boolean>();
+
+  const lightTheme = {
+    'background':'#FFFFFF',
+    'text-color':'black',
+    'light-comp':'#ececec',
+    'dark-comp':'#dddddd'
+  };
+
+  const darkTheme = {
+    'background':'#2c2c2c',
+    'text-color':'white',
+    'light-comp':'#383838',
+    'dark-comp':'#242424'
+  }
+
+  useEffect(() => {
+    const darkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (darkMode != null) {
+     setDarkMode(darkMode);
+     setThemeVariables(darkMode);
+    }
+    else{
+      setDarkMode(true);
+    }
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const setThemeVariables = (isDarkMode:boolean) =>{
+    const theme = isDarkMode? darkTheme : lightTheme;
+    document.documentElement.style.setProperty('--background',theme.background);
+    document.documentElement.style.setProperty('--text-color',theme["text-color"]);
+    document.documentElement.style.setProperty('--light-comp',theme["light-comp"]);
+    document.documentElement.style.setProperty('--dark-comp',theme["dark-comp"]);
+  }
 
   const toggleMenu = () => {
     setMenuVisible(!isMenuVisible);
   };
+
+  function toggleTheme(event: ChangeEvent<HTMLInputElement>): void {
+    setThemeVariables(event.target.checked);
+    setDarkMode(event.target.checked);
+  }
 
   return (
     <>
@@ -28,8 +71,8 @@ function Header() {
               type="checkbox"
               className="checkbox"
               id="checkbox"
-              /* checked={isDarkMode}
-              onChange={toggleTheme} */
+              checked={darkMode}
+              onChange={toggleTheme}
             />
             <label htmlFor="checkbox" className="checkbox-label">
               <i className="fas fa-moon"></i>
