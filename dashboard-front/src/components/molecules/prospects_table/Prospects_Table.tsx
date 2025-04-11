73a387bf31c777723624fa5ea1e7feeff6528cfb
jfrodriguezhD <frodriguezherrera@deloitte.com>
2025-04-10 20:11:21 -0600

    pin archive re refactor *some bugs yet*

--- dashboard-front/src/components/molecules/prospects_table/Prospects_Table.tsx
+++ dashboard-front/src/components/molecules/prospects_table/Prospects_Table.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, SetStateAction, useContext, useEffect, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { prospectBaseApiURL } from "../../../data/endpoints/api_endpoints.ts";
 import { PAGE_LIMIT } from "../../../data/general_variables/important_figures.ts";
@@ -7,29 +7,34 @@ import { Prospects_Footer_Page_Marker } from "../../atoms/prospects_footer_page_
 import { CreateNewProspect } from "../../organism/create_new_prospect/CreateNewProspect.tsx";
 import ProspectView from "../../organism/prospect_view_menu/ProspectView.tsx";
 import "./Prospects_Table.css";
-import { SearchContext, SortContext } from "../../../pages/App.tsx";
-
-export const SelectedRowContext = createContext<Dispatch<SetStateAction<number>> | undefined>(undefined);
+import { SearchContext, SelectedProspectContext, SelectedRowContext, ShowListContext, SortContext } from "../../../pages/App.tsx";
 
 function Prospects_Table() {
 
-    const [selectedRow, setSelectedRow] = useState<number>(0);
+    const {selectedRow, setSelectedRow} = useContext(SelectedRowContext);
     const [pageNumber, setPageNumber] = useState(1);
     const { page } = useParams();
 
     const [ list, setList ] = useState<Prospects[]>([])
     const [ sortList, setSortList ] = useState<Prospects[]>([])
     const [ searchList, setSearchList ] = useState<Prospects[]>([])
-    const [ showList, setShowList ] = useState<Prospects[]>([])
+    //const [ showList, setShowList ] = useState<Prospects[]>([])
 
     const profileModal = useRef<HTMLDialogElement>(null);
     const viewRef = useRef<HTMLDialogElement>(null);
 
     const searchContext = useContext(SearchContext);
   	const sortContext = useContext(SortContext);
+    const prospectContext = useContext(SelectedProspectContext);
+    const showListContext = useContext(ShowListContext);
 
 	const search = searchContext?.search;
 	const sort = sortContext?.sort;
+    const {showList, setShowList} = showListContext;
+
+    if(sortList.length > 0 && selectedRow!=-1){
+        prospectContext?.setSelectedProspect(showList[selectedRow]);
+    }
 
     function toggleView() {
         if (!viewRef.current) {
@@ -198,8 +203,7 @@ function Prospects_Table() {
             <div>Prospected For</div>
         </div>
         <div className="prospects__table__row__container">
-            {showList.length>0?<ProspectView prospect={showList[selectedRow]} toggleDialog={toggleView} ref={viewRef} />:null}
-            <SelectedRowContext.Provider value={setSelectedRow}>
+            {showList.length>0? <ProspectView prospect={showList[selectedRow!=-1?selectedRow:0]} toggleDialog={toggleView} ref={viewRef} />:null}
             {
                 showList.map((data, index) => {
                         if (index < (PAGE_LIMIT * pageNumber) && index >= ((PAGE_LIMIT * pageNumber) - PAGE_LIMIT)) {
@@ -208,7 +212,6 @@ function Prospects_Table() {
                     }
                 )
             }
-            </SelectedRowContext.Provider>
             <div className='prospects__row add__new__prospect' onClick={() => toggleDialog()}>
                 Add New Prospect
             </div>
--- dashboard-front/src/components/organism/control/Control.tsx
+++ dashboard-front/src/components/organism/control/Control.tsx
@@ -1,17 +1,106 @@
 import "./Control.css";
 import { outputs, filters, tools } from "../../../data/control/control_data";
-import { SearchContext, SortContext } from "../../../pages/App";
-import { useContext, useEffect } from "react";
+import { SearchContext, SelectedProspectContext, SelectedRowContext, ShowListContext, SortContext } from "../../../pages/App";
+import { useContext, useEffect, useState } from "react";
 import ToolButton from "../../atoms/toolbutton/ToolButton";
 import WordBubble from "../../atoms/wordbubble/WordBubble";
+import { prospectBaseApiURL } from "../../../data/endpoints/api_endpoints";
 
 export default function Control() {
 
+	const [ prospectPinList, setProspectPinList ] = useState<number[]>();
+
 	const searchContext = useContext(SearchContext);
   	const sortContext 	= useContext(SortContext);
+	const showListContext = useContext(ShowListContext);
+	const selectedProspectContext = useContext(SelectedProspectContext);
+	const selectedRowContext = useContext(SelectedRowContext);
 
 	const { search, setSearch } = searchContext;
 	const { sort, setSort } = sortContext;
+	const { showList, setShowList } = showListContext;
+	const { selectedProspect, setSelectedProspect } = selectedProspectContext;
+	const {selectedRow, setSelectedRow} = selectedRowContext;
+
+	useEffect(() => {
+		const storedPinList = localStorage.getItem('pinned-prospects')
+		const pinList = storedPinList? JSON.parse(storedPinList) : [];
+		setProspectPinList(pinList);	
+		setShowList([
+			...showList.filter(obj => prospectPinList?.includes(obj.id))
+			.sort((a,b) => prospectPinList?.indexOf(a.id) - prospectPinList?.indexOf(b.id)),
+			...showList.filter(obj => !prospectPinList?.includes(obj.id))
+		])
+		console.log(showList);
+		console.log(pinList)
+			
+		setShowList([
+			...showList.filter(obj => prospectPinList?.includes(obj.id))
+			.sort((a,b) => prospectPinList?.indexOf(a.id) - prospectPinList?.indexOf(b.id)),
+			...showList.filter(obj => !prospectPinList?.includes(obj.id))
+		]);
+	  }, []);
+	  
+	useEffect(() => {
+		localStorage.setItem('pinned-prospects', JSON.stringify(prospectPinList));
+		setShowList([
+			...showList.filter(obj => prospectPinList?.includes(obj.id))
+			.sort((a,b) => prospectPinList?.indexOf(a.id) - prospectPinList?.indexOf(b.id)),
+			...showList.filter(obj => !prospectPinList?.includes(obj.id))
+		]);
+	  }, [prospectPinList]);
+
+	const archiveProspect = () => {
+		selectedProspect.status = ["ARCHIVED"];
+	
+		const modifyURL = `${prospectBaseApiURL}/${selectedProspect.id}`;
+	
+		fetch(modifyURL, {
+		  method: "PUT",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(selectedProspect),
+		})
+		  .then((response) => response.json())
+		  .then((data) =>
+			alert("Profile successfully updated: " + JSON.stringify(data))
+		  );
+		location.reload();
+	}
+
+	const pinProspect = () => {
+		if(prospectPinList?.includes(selectedProspect.id)){
+			setProspectPinList(prospectPinList.filter(item => item !== selectedProspect.id));
+		}
+		else{
+			setProspectPinList([selectedProspect.id,...prospectPinList]);
+			setSelectedRow(0);
+		}
+	}
+	
+	const handleToolBtnClick = (e: MouseEvent, toolType:string) =>{
+		switch (toolType){
+		  case tools[0].word:
+			e?.preventDefault();
+			showModal();
+			break;
+		  case tools[1].word:
+			e?.preventDefault();
+			archiveProspect();
+			break;
+		  case tools[2].word:
+			pinProspect();
+			break;
+		}
+	  }
+	
+	const showModal = () => {
+		const modal = document.querySelector(
+		  ".prospect-modal-view"
+		) as HTMLDialogElement;
+		modal!.showModal();
+	};
 
 	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value)
@@ -68,10 +157,14 @@ export default function Control() {
 					/>
 			</div>
 		</section>
-		<section className="control__tools">
-			{/*tools.map((tool, index) => {
-			return <ToolButton word={tool.word} group="tools" icon={tool.icon} key={index}/>;
-			})*/}
+		<section className={`control__tools ${selectedRow==-1?"hidden":""}`}>
+			{tools.map((tool, index) => {
+			return <ToolButton 
+							word={tool.word}
+							group="tools" icon={tool.icon}
+							key={index}
+							handleClick={()=> handleToolBtnClick(event,tool.word)}/>;
+			})}
 		</section>
 		</div>
 	);
